57290a0b8345bdc9934300dde4dff02936073b69
Nayemul <nayemulhasan2016@gmail.com>
2021-03-13 04:17:35 +0600

    sign-in with pupup

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ firebase.initializeApp(firebaseConfig);
 function App() {
   const provider = new firebase.auth.GoogleAuthProvider();
 
+  const haldleSignin = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, email, phone,photoURL} = res.user
+      console.log(displayName, email, phone, photoURL);
+    })
+  }
   return (
     <div className="App">
-      <button>Sign in</button>
+      <button onClick={haldleSignin} >Sign in</button>
     </div>
   );
 }
